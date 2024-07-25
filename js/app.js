d3a7d641dd368a707de18021c78203f926b46a65
Fausto Alves <fausto@faustoalves.com.br>
2024-07-25 18:40:29 -0300

    InitialTest

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 mapboxgl.accessToken =
   "pk.eyJ1IjoiY2FybG9zdG9sZWRvIiwiYSI6ImNpbXZlMWRnMzAycHZ1cGtrZzQ4YW1yZ3IifQ.uG-dnwIGk10NK08Tn8fU-Q";
-var origin = [-122.43314482931885, 37.73850828742469];
+var origin = [-122.434, 37.7353];
+const P2 = (x, y) => ({x, y});
+
 var building;
 const map = new mapboxgl.Map({
   container: "map",
@@ -8,9 +10,9 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/carlostoledo/cly4imkcj00ib01qjga6t6bbw/draft",
   center: origin,
   projection: "globe",
-  zoom: 19,
+  zoom: 11,
   antialias: true,
-  pitch: 80,
+  pitch: 65,
   interactive: true,
   transitionZoomLevel: 5.9, // create the gl context with MSAA antialiasing, so custom layers are antialiased
 });
@@ -27,7 +29,7 @@ map
       type: "custom",
       renderingMode: "3d",
       onAdd: function () {
-        const scale = 4;
+        const scale = 400;
         const options = {
           obj: "/assets/odisea.gltf",
           type: "gltf",
@@ -45,21 +47,19 @@ map
 
       render: function () {
         tb.update();
-        map.rotation = {x: 90, y: 180, z: 0};
       },
     });
   })
   .on("click", function (e) {
     var pt = [e.lngLat.lng, e.lngLat.lat];
-    travelPath(pt);
+    gotoSpecificPosition(pt);
   });
 
 function onObjectChanged(e) {
   let model = e.detail.object; //here's the object already modified
   let action = e.detail.action; //here's the action that changed the object
 }
-
-function travelPath(destination) {
+function travelPath1(destination) {
   console.log(destination);
   // request directions. See https://docs.mapbox.com/api/navigation/#directions for details
 
@@ -76,12 +76,58 @@ function travelPath(destination) {
       duration: 4000,
     };
 
+    console.log("options: " + options.path);
+
     // start the truck animation with above options, and remove the line when animation ends
     building.followPath(options, function () {
       // tb.remove(line);
     });
     origin = destination;
   });
+}
+async function travelPath(destination) {
+  let points = [P2(origin[0], origin[1]), P2(destination[0], destination[1])];
+  let interpolatedPoints = interpolatePath(points, 0.1);
+
+  console.log("interpolatedPoints", interpolatedPoints);
+  let values = interpolatedPoints.map((item) => {
+    return [item.x, item.y];
+  });
+
+  if (values.length <= 2) {
+    values = [
+      [(origin[0] + destination[0]) / 2, origin[1] / destination[1] / 2],
+      [destination[0], destination[1]],
+    ];
+  } else {
+    values.push([destination[0], destination[1]]);
+  }
+  console.log();
+  console.log("values", values);
+  origin = destination;
+  building.followPath({path: values, duration: 1000}, function (err, result) {
+    building.setCoords(destination);
+  });
+
+  // var url =
+  //   "https://api.mapbox.com/directions/v5/mapbox/walking/" +
+  //   [origin, destination].join(";") +
+  //   "?geometries=geojson&access_token=" +
+  //   "pk.eyJ1IjoiY2FybG9zdG9sZWRvIiwiYSI6ImNpbXZlMWRnMzAycHZ1cGtrZzQ4YW1yZ3IifQ.uG-dnwIGk10NK08Tn8fU-Q";
+
+  // fetchFunction(url, function (data) {
+  //   // extract path geometry from callback geojson, and set duration of travel
+  //   var options = {
+  //     path: data.routes[0].geometry.coordinates,
+  //     duration: 4000,
+  //   };
+
+  //   // start the truck animation with above options, and remove the line when animation ends
+  //   building.followPath(options, function () {
+  //     // tb.remove(line);
+  //   });
+  //   origin = destination;
+  // });
 }
 
 //convenience function for fetch
@@ -102,15 +148,15 @@ btn3.addEventListener("click", function () {
   gotoSpecificPosition([-122.41986100784132, 37.699307492595025], 18);
 });
 
-function gotoSpecificPosition(destination, zoom) {
+function gotoSpecificPosition(destination) {
   travelPath(destination);
   map.flyTo({
     center: destination,
-    speed: 0.5,
-    curve: 0.5,
-    zoom: zoom,
+    speed: 1,
+    curve: 1,
+    zoom: 11,
     bearing: Math.random() * 360,
-    pitch: Math.random() * 30 + 40,
+    pitch: Math.random() * 50 + 30,
   });
 }
 
@@ -122,4 +168,32 @@ function fetchFunction(url, cb) {
       });
     }
   });
+}
+
+function interpolatePath(path, pixelStep) {
+  const res = [];
+  var p2,
+    i = 1,
+    overflow = 0;
+  while (i < path.length) {
+    const p1 = path[i - 1];
+    p2 = path[i];
+    const dx = p2.x - p1.x;
+    const dy = p2.y - p1.y;
+    const len = Math.hypot(dx, dy) / pixelStep;
+    let j = overflow;
+    while (j < len) {
+      const u = j / len;
+      res.push(P2(p1.x + dx * u, p1.y + dy * u));
+      j++;
+    }
+    overflow = j - len;
+    i++;
+  }
+
+  // add last point if close to correct distance
+  if (Math.abs(overflow) < 0.01) {
+    res.push(P2(p2.x, p2.y));
+  }
+  return res;
 }
