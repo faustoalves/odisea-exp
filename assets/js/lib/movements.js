34d6e229b47ab9820ecf0f31a3aa77df5668d780
Fausto Alves <fausto@faustoalves.com.br>
2024-08-07 22:55:42 -0300

    New paths

--- assets/js/lib/movements.js
+++ assets/js/lib/movements.js
@@ -1,13 +1,10 @@
-import { actualPosition, map, objects3d, setActualPosition } from "../app.js";
-import { generatePath, getCameraParameters, getPosition } from "./positions.js";
+import {actualPosition, map, objects3d, setActualPosition} from "../app.js";
+import {generatePath, getCameraParameters, getPosition} from "./positions.js";
 
 export function moveToPosition(position) {
   if (position !== actualPosition.location) {
     let values = getCameraParameters(position);
-    let path = generatePath(
-      getPosition(actualPosition.location),
-      getPosition(position)
-    );
+    let path = generatePath(getPosition(actualPosition.location), getPosition(position));
     let initialPath = path.slice(0, 10);
     let middlePath = path.slice(10, path.length - 10);
     let finalPath = path.slice(path.length - 10, path.length);
@@ -42,32 +39,23 @@ export function moveToPosition(position) {
       actualPosition.coordinates[0] + Math.random() * 0.1,
       actualPosition.coordinates[1] + Math.random() * 0.1,
     ]);
-    objects3d.boat.followPath(
-      { path: initialPath, duration: 3000 },
-      function () {
-        objects3d.boat.followPath(
-          { path: middlePath, duration: duration - 5000 },
-          function () {
-            objects3d.boat.followPath(
-              { path: finalPath, duration: 3000 },
-              function () {
-                objects3d.boat.stop();
-                tb.remove(line);
-                tb.remove(line2);
-                setActualPosition(getCameraParameters(position));
-              }
-            );
-          }
-        );
-      }
-    );
-    objects3d.boat.playAnimation({ animation: 0, duration: duration + 5000 });
+    objects3d.boat.followPath({path: initialPath, duration: 3000}, function () {
+      objects3d.boat.followPath({path: middlePath, duration: duration - 5000}, function () {
+        objects3d.boat.followPath({path: finalPath, duration: 3000}, function () {
+          objects3d.boat.stop();
+          tb.remove(line);
+          tb.remove(line2);
+          setActualPosition(getCameraParameters(position));
+        });
+      });
+    });
+    objects3d.boat.playAnimation({animation: 0, duration: duration + 5000});
 
     map.flyTo({
       center: values.coordinates,
       duration: duration,
       essential: true,
-      curve: 1.75,
+      curve: 0.75,
       zoom: values.zoom,
       bearing: values.bearing,
       pitch: values.pitch,
